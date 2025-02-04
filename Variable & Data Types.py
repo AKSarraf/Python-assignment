da0479389a2d14075bd6061a7307cd3a77a2d948
Ankit Kumar <162563510+AKSarraf@users.noreply.github.com>
2025-02-04 08:35:11 +0530

    Add files via upload

---
+++ Variable & Data Types.py
@@ -0,0 +1,14 @@
+# Q1: Write a Python programme that print following text exactly as it appears:
+
+#Python is fun.
+# "Quotes" and 'single quotes' can be tricky.
+
+# print('''Python is fun.\n"Quotes" and 'single quotes can be tricky''')
+
+# Q2: For a Business create 3 variables to stores- name, age, city.
+# Then print a sentence that uses these variables.
+
+name = "Ankit"
+age = 22
+city = "Ghaziabad"
+print(f"My name is {name}. And, my age is {age}. And, I am belong from {city}.")
